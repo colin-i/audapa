382de9bc037520f6bcd1759110551340f34bce73
bot <costin.botescu@gmail.com>
2021-04-04 16:49:46 +0300

    load less

--- audapa/main.py
+++ audapa/main.py
@@ -2,37 +2,23 @@ import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk,GLib
 
-import time
-
 from . import sets
 
-a=0
-def delay(w):
-	print('z')
-	global a
-	if a==2:
-		return False
-	elif a==1:
-		w.set_visible(True)
-		time.sleep(1)
-		print('q')
-		w.map()
-		time.sleep(1)
-		print('w')
-	else:
-		w.realize()
-	a+=1
-	return True
-def cl(w,d):
-	mainloop.quit()
+def cl(b,data):
+	data.quit()
+
 sets.start()
 win = Gtk.Window()
-win.set_title('Audapa')
-win.connect('close-request', cl, None)
-b=Gtk.Button.new_with_label(chr(0x2699))
-b.connect('clicked', sets.sets, win)
-win.set_child(b)
+win.set_decorated(False)#such a heavy load here if True
 win.maximize()
+bx=Gtk.Box()
+b=Gtk.Button.new_with_label(chr(0x2699))
+b.connect('clicked', sets.sets, [win,bx])
+bx.append(b)
 mainloop = GLib.MainLoop()
-GLib.timeout_add(5000,delay,win)
+b=Gtk.Button.new_with_label("X")
+b.connect('clicked', cl, mainloop)
+bx.append(b)
+win.set_child(bx)
+win.show()
 mainloop.run()
--- audapa/sets.py
+++ audapa/sets.py
@@ -15,30 +15,33 @@ color=Gtk.EntryBuffer()
 
 def reset(b,combo):
 	config = configparser.ConfigParser()
-	config['color']=color.get_text()
+	config['conf']={}
+	c=config['conf']
+	c['color']=color.get_text()
 	with open(get_config_file(), "w") as configfile:
 		config.write(configfile)
 	combo[0].set_child(combo[1])
-def sets(b,w):
+def sets(b,combo):
 	bx=Gtk.Box()
 	bx.set_orientation(Gtk.Orientation.VERTICAL)
 	en=colorEntry(color)
 	bx.append(en)
 	b=Gtk.Button.new_with_label("Done")
-	b.connect('clicked', reset, [w,b])
+	b.connect('clicked', reset, combo)
 	bx.append(b)
-	w.set_child(bx)
+	combo[0].set_child(bx)
 
 def colorEntry(b):
 	en=Gtk.Entry.new_with_buffer(b)
 	if (c:=color.get_text()):
 		cont=en.get_style_context()
 		p=Gtk.CssProvider()
-		p.load_from_data (b"entry { color: "+c+"; }")
+		p.load_from_data (b"entry { color: "+c.encode()+b"; }")
 		cont.add_provider(p,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 	return en
 
 def start():
 	config = configparser.ConfigParser()
 	if(config.read(get_config_file())):
-		color.set_text(config['color'])
+		c=config['conf']
+		color.set_text(c['color'],-1)
