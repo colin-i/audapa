d299163d1d7c7069caddeac5e1e5d26280e6b4d5
bot <costin.botescu@gmail.com>
2021-04-08 19:30:17 +0300

    info

---
+++ setup-req-test.py
@@ -0,0 +1,11 @@
+
+import subprocess
+import sys
+test=subprocess.run([sys.executable,'-m','pip','install','PyGObject>=3.40'])
+if test.returncode:
+	exit(test.returncode)
+import gi
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk,GLib
+
+subprocess.run([sys.executable,'-m','pip','install','--user','.'])
--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 pkname='audapa'
 
-import subprocess
-import sys
-test=subprocess.run([sys.executable,'-m','pip','install','PyGObject>=3.40'])
-if test.returncode:
-	exit(test.returncode)
-import gi
-gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk,GLib
-
 from setuptools import setup
 setup(name=pkname,
 	version='1.0.0',
