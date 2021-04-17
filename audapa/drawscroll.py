65ee8a786c673ee335b0a36288c45fd312e393b8
bot <costin.botescu@gmail.com>
2021-04-17 09:20:30 +0300

    fix

--- audapa/drawscroll.py
+++ audapa/drawscroll.py
@@ -7,16 +7,24 @@ def calculate(n):
 	w=win.get_width()
 	h=win.get_height()
 	d=draw.area
+	global size
+	f=3
 	if w>=h:
 		#30000 maximum size of an X window
-		if n>(size:=(w*2)):
+		if n>(size:=(w*f)):
 			n=size
+		elif n<=w:#cannot go back at overshot without this
+			if draw.length>w:
+				n=w+1
 		if d.get_width()!=n or d.get_height()!=h:
 			d.set_size_request(n,h)
 			return True
 	else:
-		if n>(size:=(h*2)):
+		if n>(size:=(h*f)):
 			n=size
+		elif n<=h:
+			if draw.length>h:
+				n=h+1
 		if d.get_width()!=w or d.get_height()!=n:
 			d.set_size_request(w,n)
 			return True
@@ -26,23 +34,23 @@ def edge(wn,pos,d):
 	if pos==Gtk.PositionType.RIGHT:
 		x=draw.offset+draw.size
 		if x<draw.length:
-			draw.offset+=win.get_width()
+			draw.offset+=size
 			r_offset.cged(win.get_hadjustment())
 			draw.area.queue_draw()
 	elif pos==Gtk.PositionType.BOTTOM:
-		x=draw.offset+draw.size
-		if x<draw.length:
-			draw.offset+=win.get_height()
+		y=draw.offset+draw.size
+		if y<draw.length:
+			draw.offset+=size
 			r_offset.cged(win.get_vadjustment())
 			draw.area.queue_draw()
 	elif pos==Gtk.PositionType.LEFT:
 		if draw.offset>0:
-			draw.offset-=min(win.get_width(),draw.offset)#can be unfixed
+			draw.offset-=min(size,draw.offset)#can be unfixed
 			r_offset.cged(win.get_hadjustment())
 			draw.area.queue_draw()
 	else:
 		if draw.offset>0:
-			draw.offset-=min(win.get_height(),draw.offset)
+			draw.offset-=min(size,draw.offset)
 			r_offset.cged(win.get_vadjustment())
 			draw.area.queue_draw()
 
