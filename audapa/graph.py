d222425a06404f5e7c6c93030c1b17d88d87f254
costin.botescu@gmail.com <costin.botescu@gmail.com>
2021-07-25 15:46:54 +0300

    adjustments

--- audapa/graph.py
+++ audapa/graph.py
@@ -1,6 +1,7 @@
 
 from gi.repository import Gtk,Gdk
 import cairo
+import math
 
 from . import points
 from . import sets
@@ -23,11 +24,24 @@ def put(ix,c1,w,h):
 	if ix>0:
 		c0=points.points[ix-1]._coord_(w,h)
 		line(c0,c1)
+	elif len(points.points)>1:
+		c0=points.points[1]._coord_(w,h)
+		line(c1,c0)
 def line(c0,c1):
 	cr=cairo.Context(surface)
 	co=Gdk.RGBA()
 	if co.parse(sets.get_fgcolor2()):
 		cr.set_source_rgb(co.red,co.green,co.blue)
-	cr.move_to(c0[0],c0[1])
-	cr.line_to(c1[0],c1[1])
+	line_draw(cr,c0,c1)
 	cr.stroke()
+#don't let line width extremities to mess when deleting
+def line_draw(cr,c0,c1):
+	x=c1[0]-c0[0]
+	y=c1[1]-c0[1]
+	l=cr.get_line_width()
+	t=x/y
+	r=math.atan(t)
+	x=math.sin(r)*l
+	y=math.cos(r)*l
+	cr.move_to(c0[0]+x,c0[1]+y)
+	cr.line_to(c1[0]-x,c1[1]-y)
