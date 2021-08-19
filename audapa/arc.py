4ae1643785c0eb0c213509575334ad8fca3a812f
costin.botescu@gmail.com <costin.botescu@gmail.com>
2021-08-19 12:06:05 +0300

    convex

--- audapa/arc.py
+++ audapa/arc.py
@@ -9,29 +9,34 @@ def draw(cr,x0,y0,x1,y1):
 	if land:
 		x=x1-x0
 		y=abs(y1-y0)
-		if x>y:
-			rads=graph.rads(x,y)
-			aux=math.cos(rads)
-			cat=y/aux
-			#
-			c=(cat/2)/aux
-			raddif=(math.pi/2-rads)*2
-			if y0<y1:
-				cr.arc(x0,y0+c,c,math.pi*3/2,math.pi*3/2+raddif)
-			else:
-				cr.arc(x1,y1+c,c,math.pi*3/2-raddif,math.pi*3/2)
-		else:
-			rads=graph.rads(y,x)
-			aux=math.cos(rads)
-			cat=x/aux
-			#
-			c=(cat/2)/aux
-			raddif=(math.pi/2-rads)*2
-			if y0<y1:
-				cr.arc(x1-c,y1,c,-raddif,0)
-			else:
-				cr.arc(x0+c,y0,c,math.pi,math.pi+raddif)
 	else:
-		pass
-		#x=abs(x1-xo)
-		#y=y1-y0
+		x=abs(x1-x0)
+		y=y1-y0
+	if x>y:
+		rads=graph.rads(x,y)
+		aux=math.cos(rads)
+		cat=y/aux
+		#
+		c=(cat/2)/aux
+		raddif=(math.pi/2-rads)*2
+		if (land and y0<y1) or (x0<x1 and land==False):
+			cr.arc(x0,y0+c,c,math.pi*3/2,math.pi*3/2+raddif)
+			return
+		if land:
+			cr.arc(x1,y1+c,c,math.pi*3/2-raddif,math.pi*3/2)
+			return
+		cr.arc(x1,y1-c,c,math.pi/2-raddif,math.pi/2)
+		return
+	rads=graph.rads(y,x)
+	aux=math.cos(rads)
+	cat=x/aux
+	#
+	c=(cat/2)/aux
+	raddif=(math.pi/2-rads)*2
+	if (land and y0<y1) or (x0<x1 and land==False):
+		cr.arc(x1-c,y1,c,-raddif,0)
+		return
+	if land:
+		cr.arc(x0+c,y0,c,math.pi,math.pi+raddif)
+		return
+	cr.arc(x0-c,y0,c,0,raddif)
