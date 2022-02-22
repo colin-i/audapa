5b3749f355f0e6ee41a58002bc1a71fbb43d6f05
bc <costin.botescu@gmail.com>
2022-02-22 15:55:10 +0200

    fix arc

--- audapa/arc.py
+++ audapa/arc.py
@@ -14,40 +14,94 @@ def draw(cr,x0,y0,x1,y1,convex):
 		x=abs(x1-x0)
 		y=y1-y0
 	c,raddif,radsmall=vals(x,y)
-	if convex:
+	if convex: #convex on land
 		if x>y:
-			if (land and y0<y1) or (x0<x1 and land==False):
-				arc(cr,x0,y0+c,c,math.pi*3/2,math.pi*3/2+raddif,radsmall)
-				return
 			if land:
-				arc(cr,x1,y1+c,c,math.pi*3/2-raddif,math.pi*3/2,radsmall)
-				return
+				if y0<y1:
+					#...
+					#   ...
+					arc(cr,x0,y0+c,c,math.pi*3/2,math.pi*3/2+raddif,radsmall)
+				else:
+					#   ...
+					#...
+					arc(cr,x1,y1+c,c,math.pi*3/2-raddif,math.pi*3/2,radsmall)
+			elif x0<x1:
+				#...
+				#   ...
+				arc(cr,x1,y1-c,c,math.pi/2,math.pi/2+raddif,radsmall)
+			else:
+				#   ...
+				#...
+				arc(cr,x0,y0+c,c,math.pi*3/2-raddif,math.pi*3/2,radsmall)
+		elif land:
+			if y0<y1:
+				#.
+				#.
+				# .
+				# .
+				arc(cr,x1-c,y1,c,-raddif,0,radsmall)
+			else:
+				# .
+				# .
+				#.
+				#.
+				arc(cr,x0+c,y0,c,math.pi,math.pi+raddif,radsmall)
+		else:
+			if x0<x1:
+				#.
+				#.
+				# .
+				# .
+				arc(cr,x0+c,y0,c,math.pi-raddif,math.pi,radsmall)
+			else:
+				# .
+				# .
+				#.
+				#.
+				arc(cr,x1+c,y1,c,math.pi,math.pi+raddif,radsmall)
+	elif x>y:
+		if land:
+			if y0<y1:
+				#...
+				#   ...
+				arc(cr,x1,y1-c,c,math.pi/2,math.pi/2+raddif,radsmall)
+			else:
+				#   ...
+				#...
+				arc(cr,x0,y0-c,c,math.pi/2-raddif,math.pi/2,radsmall)
+		elif x0<x1:
+			#...
+			#   ...
+			arc(cr,x0,y0+c,c,math.pi*3/2,math.pi*3/2+raddif,radsmall)
+		else:
+			#   ...
+			#...
 			arc(cr,x1,y1-c,c,math.pi/2-raddif,math.pi/2,radsmall)
-			return
-		if (land and y0<y1) or (x0<x1 and land==False):
-			arc(cr,x1-c,y1,c,-raddif,0,radsmall)
-			return
-		if land:
-			arc(cr,x0+c,y0,c,math.pi,math.pi+raddif,radsmall)
-			return
+	elif land:
+		if y0<y1:
+			#.
+			#.
+			# .
+			# .
+			arc(cr,x0+c,y0,c,math.pi-raddif,math.pi,radsmall)
+		else:
+			# .
+			# .
+			#.
+			#.
+			arc(cr,x1-c,y1,c,0,raddif,radsmall)
+	elif x0<x1:
+		#.
+		#.
+		# .
+		# .
+		arc(cr,x1-c,y1,c,-raddif,0,radsmall)
+	else:
+		# .
+		# .
+		#.
+		#.
 		arc(cr,x0-c,y0,c,0,raddif,radsmall)
-		return
-	if x>y:
-		if (land and y0<y1) or (x0<x1 and land==False):
-			arc(cr,x1,y1-c,c,math.pi/2,math.pi/2+raddif,radsmall)
-			return
-		if land:
-			arc(cr,x0,y0-c,c,math.pi/2-raddif,math.pi/2,radsmall)
-			return
-		arc(cr,x0,y0+c,c,math.pi*3/2-raddif,math.pi*3/2,radsmall)
-		return
-	if (land and y0<y1) or (x0<x1 and land==False):
-		arc(cr,x0+c,y0,c,math.pi-raddif,math.pi,radsmall)
-		return
-	if land:
-		arc(cr,x1-c,y1,c,0,raddif,radsmall)
-		return
-	arc(cr,x1+c,y1,c,math.pi,math.pi+raddif,radsmall)
 def vals(x,y):
 	l=point.const
 	if x>y:
