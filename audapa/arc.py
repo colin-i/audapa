93035d1eb90f6c2ee5bb310c078a2cd2d98a2779
bc <costin.botescu@gmail.com>
2022-02-24 16:09:25 +0200

    steps at save

--- audapa/arc.py
+++ audapa/arc.py
@@ -14,117 +14,164 @@ def draw(cr,x0,y0,x1,y1,convex):
 		x=abs(x1-x0)
 		y=y1-y0
 	c,raddif,radsmall=vals(x,y)
-	if convex: #convex on land
-		if x>y:
-			if land:
+	xc,yc,rstart,rend=center(x0,y0,x1,y1,convex,x,y,c,land,raddif)
+	cr.arc(xc,yc,c,rstart+radsmall,rend-radsmall)
+def center(x0,y0,x1,y1,convex,x,y,c,land=True,raddif=None):
+	if land:
+		if convex: #convex on land
+			if x>y:
 				if y0<y1:
 					#...
 					#   ...
-					arc(cr,x0,y0+c,c,math.pi*3/2,math.pi*3/2+raddif,radsmall)
+					xc=x0
+					yc=y0+c
+					if raddif!=None:
+						return (xc,yc,math.pi*3/2,math.pi*3/2+raddif)
 				else:
 					#   ...
 					#...
-					arc(cr,x1,y1+c,c,math.pi*3/2-raddif,math.pi*3/2,radsmall)
+					xc=x1
+					yc=y1+c
+					if raddif!=None:
+						return (xc,yc,math.pi*3/2-raddif,math.pi*3/2)
 			else:
-				if x0<x1:
-					#...
-					#   ...
-					arc(cr,x1,y1-c,c,math.pi/2,math.pi/2+raddif,radsmall)
+				if y0<y1:
+					#.
+					#.
+					# .
+					# .
+					xc=x1-c
+					yc=y1
+					if raddif!=None:
+						return (xc,yc,-raddif,0)
 				else:
-					#   ...
-					#...
-					arc(cr,x0,y0+c,c,math.pi*3/2-raddif,math.pi*3/2,radsmall)
+					# .
+					# .
+					#.
+					#.
+					xc=x0+c
+					yc=y0
+					if raddif!=None:
+						return (xc,yc,math.pi,math.pi+raddif)
 		else:
-			if land:
+			if x>y:
+				if y0<y1:
+					#...
+					#   ...
+					xc=x1
+					yc=y1-c
+					if raddif!=None:
+						return (xc,yc,math.pi/2,math.pi/2+raddif)
+				else:
+					#   ...
+					#...
+					xc=x0
+					yc=y0-c
+					if raddif!=None:
+						return (xc,yc,math.pi/2-raddif,math.pi/2)
+			else:
 				if y0<y1:
 					#.
 					#.
 					# .
 					# .
-					arc(cr,x1-c,y1,c,-raddif,0,radsmall)
+					xc=x0+c
+					yc=y0
+					if raddif!=None:
+						return (xc,yc,math.pi-raddif,math.pi)
 				else:
 					# .
 					# .
 					#.
 					#.
-					arc(cr,x0+c,y0,c,math.pi,math.pi+raddif,radsmall)
-			else:
-				if x0<x1:
-					#.
-					#.
-					# .
-					# .
-					arc(cr,x0+c,y0,c,math.pi-raddif,math.pi,radsmall)
-				else:
-					# .
-					# .
-					#.
-					#.
-					arc(cr,x1+c,y1,c,math.pi,math.pi+raddif,radsmall)
+					xc=x1-c
+					yc=y1
+					if raddif!=None:
+						return (xc,yc,0,raddif)
 	else:
-		if x>y:
-			if land:
-				if y0<y1:
-					#...
-					#   ...
-					arc(cr,x1,y1-c,c,math.pi/2,math.pi/2+raddif,radsmall)
-				else:
-					#   ...
-					#...
-					arc(cr,x0,y0-c,c,math.pi/2-raddif,math.pi/2,radsmall)
-			else:
+		if convex:
+			if x>y:
 				if x0<x1:
 					#...
 					#   ...
-					arc(cr,x0,y0+c,c,math.pi*3/2,math.pi*3/2+raddif,radsmall)
+					xc=x1
+					yc=y1-c
+					return (xc,yc,math.pi/2,math.pi/2+raddif)
 				else:
 					#   ...
 					#...
-					arc(cr,x1,y1-c,c,math.pi/2-raddif,math.pi/2,radsmall)
+					xc=x0
+					yc=y0+c
+					return (xc,yc,math.pi*3/2-raddif,math.pi*3/2)
+			else:
+				if x0<x1:
+					#.
+					#.
+					# .
+					# .
+					xc=x0+c
+					yc=y0
+					return (xc,yc,math.pi-raddif,math.pi)
+				else:
+					# .
+					# .
+					#.
+					#.
+					xc=x1+c
+					yc=y1
+					return (xc,yc,math.pi,math.pi+raddif)
 		else:
-			if land:
-				if y0<y1:
-					#.
-					#.
-					# .
-					# .
-					arc(cr,x0+c,y0,c,math.pi-raddif,math.pi,radsmall)
+			if x>y:
+				if x0<x1:
+					#...
+					#   ...
+					xc=x0
+					yc=y0+c
+					return (xc,yc,math.pi*3/2,math.pi*3/2+raddif)
 				else:
-					# .
-					# .
-					#.
-					#.
-					arc(cr,x1-c,y1,c,0,raddif,radsmall)
+					#   ...
+					#...
+					xc=x1
+					yc=y1-c
+					return (xc,yc,math.pi/2-raddif,math.pi/2)
 			else:
 				if x0<x1:
 					#.
 					#.
 					# .
 					# .
-					arc(cr,x1-c,y1,c,-raddif,0,radsmall)
+					xc=x1-c
+					yc=y1
+					return (xc,yc,-raddif,0)
 				else:
 					# .
 					# .
 					#.
 					#.
-					arc(cr,x0-c,y0,c,0,raddif,radsmall)
-def vals(x,y):
-	l=point.const
+					xc=x0-c
+					yc=y0
+					return (xc,yc,0,raddif)
+	return (xc,yc)
+def radius(x,y):
 	if x>y:
 		rads=graph.rads(x,y)
 		aux=math.cos(rads)
 		cat=y/aux
-		ad=aux*l  #ysmall
-		op=math.sin(rads)*l  #xsmall
 	else:
 		rads=graph.rads(y,x)
 		aux=math.cos(rads)
 		cat=x/aux
-		ad=aux*l  #xsmall
-		op=math.sin(rads)*l  #ysmall
 	c=(cat/2)/aux
+	return (c,rads,aux)
+def vals(x,y):
+	c,rads,aux=radius(x,y)
+	l=point.const
+	if x>y:
+		ad=aux*l             #ysmall
+		op=math.sin(rads)*l  #xsmall
+	else:
+		ad=aux*l             #xsmall
+		op=math.sin(rads)*l  #ysmall
 	radsmall=math.atan2(op,c-ad)
 	raddif=(math.pi/2-rads)*2
 	return (c,raddif,radsmall)
-def arc(cr,x,y,c,a,b,radsmall):
-	cr.arc(x,y,c,a+radsmall,b-radsmall)
--- audapa/save.py
+++ audapa/save.py
@@ -1,9 +1,24 @@
 
 from . import points
+from . import arc
 
 def data(b,d):
-	for p in points.points:
-		#next off
-		#angle known
-		#height known
-		pass
+	s=len(points.points)
+	for i in range(1,s):
+		prev=points.points[i-1]
+		cur=points.points[i]
+		x0=prev._offset_
+		y0=prev._height_
+		x1=cur._offset_
+		y1=cur._height_
+		x=x1-x0
+		y=y1-y0
+		#get radius
+		radius=arc.radius(x,y)[0]
+		#get center
+		xc,yc=arc.center(x0,y0,x1,y1,prev._convex_,x,y,radius)
+		#iterate
+		for i in range(x0,x1):
+			i-=xc #from here is float, cannot for with floats
+			#angle knew
+			#height knew
