1f9695b93c5f2c6192beb9925b07e41b11d4269a
bc <costin.botescu@gmail.com>
2022-03-04 09:03:50 +0200

    set at loud

--- audapa/level.py
+++ audapa/level.py
@@ -2,14 +2,29 @@
 from gi.repository import Gtk
 
 from . import sets
+from . import draw
+
+dif=Gtk.EntryBuffer()
 
 def open(b,combo):
 	box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-	en=sets.colorEntry()
+	#+/- button or not   entry   maxim
+	b2=Gtk.Box()
+	if draw.baseline!=0:
+		s=sets.colorButton("+",sign,"Sign")
+		b2.append(s)
+	en=sets.colorEntry(dif)
+	b2.append(en)
+	box.append(b2)
+	#atstart   middle   - or calculated
+	#Calculate
+	#Cancel
 	bt=sets.colorButton("Done",click,"Return",combo)
-	box.append(en)
 	box.append(bt)
 	combo[0].set_child(box)
 
 def click(b,combo):
 	combo[0].set_child(combo[1])
+
+def sign(b,d):
+	pass
