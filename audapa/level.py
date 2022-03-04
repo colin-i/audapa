d8e0aab5dfc1a6629217cf0e8d62413e051ed573
bc <costin.botescu@gmail.com>
2022-03-04 15:26:58 +0200

    boxes

--- audapa/level.py
+++ audapa/level.py
@@ -15,11 +15,25 @@ def open(b,combo):
 		b2.append(s)
 	en=sets.colorEntry(dif)
 	b2.append(en)
+	mx=sets.colorLabel("0")
+	b2.append(mx)
 	box.append(b2)
-	#atstart   middle   - or calculated
+	#atstart   middle[-1,1]   - or calculated
+	st=sets.colorLabel("0")
+	md=sets.colorLabel("0")
+	cal=sets.colorLabel("0")
+	b3=Gtk.Box()
+	b3.append(st)
+	b3.append(md)
+	b3.append(cal)
+	box.append(b3)
 	#Calculate
+	calc=sets.colorButton("Calculate",calcs,"Test")
+	box.append(calc)
 	#Cancel
-	bt=sets.colorButton("Done",click,"Return",combo)
+	exit=sets.colorButton("Cancel",abort,"Abort")
+	box.append(exit)
+	bt=sets.colorButton("Done",click,"Apply",combo)
 	box.append(bt)
 	combo[0].set_child(box)
 
@@ -27,4 +41,10 @@ def click(b,combo):
 	combo[0].set_child(combo[1])
 
 def sign(b,d):
+	pass
+
+def calcs(b,d):
+	pass
+
+def abort(b,d):
 	pass
