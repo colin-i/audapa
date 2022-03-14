883429301de01e925f1bd69bceac934a2deb5bf5
bc <costin.botescu@gmail.com>
2022-03-14 15:44:52 +0200

    preset size at spread

--- audapa/spread.py
+++ audapa/spread.py
@@ -4,16 +4,18 @@ from gi.repository import Gtk
 from . import sets
 from . import points
 from . import level
+from . import draw
+from . import blank
 
 spread=Gtk.EntryBuffer()
-compress=Gtk.CheckButton()
+reduce=Gtk.CheckButton()
 
 def open(b,combo):
 	bx=Gtk.Grid(hexpand=True)
 	bx.attach(sets.colorLabel("Spread/Compress N samples"),0,0,1,1)
 	bx.attach(sets.colorEntry(spread),1,0,1,1)
 	bx.attach(sets.colorLabel("Compress"),0,1,1,1) #Enlarge
-	bx.attach(compress,1,1,1,1)
+	bx.attach(reduce,1,1,1,1)
 	bx.attach(sets.colorButton("Cancel",cancel,"Abort",combo),0,2,2,1)
 	bx.attach(sets.colorButton("Done",done,"Apply",combo),0,3,2,1)
 	combo[0].set_child(bx)
@@ -29,30 +31,40 @@ def done(b,combo):
 		if s>=2:
 			b=int(a)
 			n=ps[s-1]._offset_-ps[0]._offset_
-			if compress.get_active():
+			if reduce.get_active():
 				if b>n:
 					spread.set_text(n.__str__(),-1)
 					return
 				#apply(-b,-1)
-				#compress()
-			#else:
-				#enlarge()
+				compress(b)
+			else:
+				enlarge(b)
 				#apply(b,1)
 			#save.apply()
+			combo[0].set_child(combo[1])
 			#move.saved(combo)
-			#blank.saved()
-			#return
+			blank.saved()
+			return
 		combo[0].set_child(combo[1])
 		return
 	level.not_a_digit(spread)
 
-#def enlarge():
-	#1.copyout/copyback
-	#this 2.interblank/append
+def enlarge(n):
+	s=len(draw.samples)
+	extra=s-points.points[len(points.points)-1]._offset_
+	#there is a nicer move but this is lazzy
+	right=draw.samples[s-extra:]
+	draw.samples=draw.samples+([0]*n)+right
 
-#def compress():
-	#copy
-	#remove
+def compress(n):
+	#copy right at position
+	s=len(draw.samples)
+	end=points.points[len(points.points)-1]._offset_
+	extra=s-end
+	for i in range(end,s):
+		draw.samples[i-n]=draw.samples[i]
+	#remove N in safe
+	del draw.samples[s-n:]
 
 #def apply(n,sign):
 	#sz=[i]-[i-1]
