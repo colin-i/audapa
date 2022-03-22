15700a1d7b753131d98f066f2a74ecf6f8c924cb
bc <costin.botescu@gmail.com>
2022-03-22 15:30:02 +0200

    recurse on placed

--- audapa/distance.py
+++ audapa/distance.py
@@ -1,16 +1,11 @@
 
+from . import draw
 
+def test(x,y,p):
+	a=draw.cont.get_last_child() #iterate from last to first
+	recurse(x,y,p,a)
+	return True
 
-
-def distance(x,y,p):
-	#recurse(0,len,x,y,p)
-	pass
-
-def recurse(start,stop,x,y,p):
-	if start!=stop:
-		a=start+int((stop-start)/2)
-		#to_left=test(a)
-		if to_left:
-			recurse(start,a)
-		else:
-			recurse(a,stop)
+def recurse(x,y,p,a):
+	if a:
+		recurse(x,y,p,a.get_prev_sibling())
