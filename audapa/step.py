49fb4a39234cd433bc075dd84cda0a0fcada0f85
bc <costin.botescu@gmail.com>
2022-03-24 15:29:51 +0200

    step arrange

--- audapa/step.py
+++ audapa/step.py
@@ -4,19 +4,17 @@ from . import points
 from . import draw
 from . import drawscroll
 
-def left(b,d):
+def left_type():
 	p=point.lastselect
 	ix=points.points.index(p)
 	if ix!=0:
 		x,y=draw.cont.get_child_position(draw.cont.get_prev_child(p))
 		if drawscroll.landscape:
-			limit=x
+			return x
 		else:
-			limit=y
-	else:
-		limit=0
-
-def right(b,d):
+			return y
+	return 0
+def right_type():
 	p=point.lastselect
 	ix=points.points.index(p)
 	if x+1==len(points.points) or points.points[x+1]._offset_>(draw.offset+draw.size):
@@ -24,10 +22,26 @@ def right(b,d):
 	else:
 		limit=points.points[x+1]._offset_-draw._offset_
 
+def left(b,d):
+	if drawscroll.landscape:
+		limit=left_type()
+	else:
+		limit=0
+
+def right(b,d):
+	if drawscroll.landscape()
+		limit=right_type()
+	else:
+		limit=draw.area.get_width()
+
 def up(b,d):
-	p=point.lastselect
-	limit=0
+	if drawscroll.landscape:
+		limit=0
+	else:
+		limit=left_type()
 
 def down(b,d):
-	p=point.lastselect
-	limit=draw.area.get_height() if drawscroll.landscape else draw.area.get_width()
+	if drawscroll.landscape:
+		limit=draw.area.get_height()
+	else:
+		limit=right_type()
