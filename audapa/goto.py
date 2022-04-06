7e3f7d5aeb7c99f20a74fb29aed4aafa87077eac
bc <costin.botescu@gmail.com>
2022-04-06 19:40:10 +0300

    more funcs at goto

--- audapa/goto.py
+++ audapa/goto.py
@@ -7,15 +7,18 @@ from . import sets
 def open(b,d):
 	box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 	bx=Gtk.Box()
-	bx.append(sets.colorLabel("0-"))
-	x=get_max()
-	max=sets.colorLabel(x.__str__())
-	bx.append(max)
+	cur,max=get_vals()
+	bx.append(sets.colorLabel(cur.__str__()+" 0-"))
+	x=max.__str__()
+	maxlab=sets.colorLabel(x)
+	bx.append(maxlab)
 	buf=Gtk.EntryBuffer()
 	bx.append(sets.colorEntry(buf))
 	bx.append(sets.colorButton("Go",cancel,"Proceed",[max,buf]))
 	box.append(bx)
 	box.append(sets.colorButton("First",cancel,"Start"))
+	box.append(sets.colorButton("Previous",cancel,"0" if cur==0 else (cur-1).__str__()))
+	box.append(sets.colorButton("Next",cancel,x if cur==max else (cur+1).__str__(),max))
 	box.append(sets.colorButton("Last",cancel,"End",max))
 	box.append(sets.colorButton("Cancel",cancel,"Abort"))
 	info.win.set_child(box)
@@ -23,5 +26,5 @@ def open(b,d):
 def cancel(b,combo):
 	info.win.set_child(info.box)
 
-def get_max():
-	return 0
+def get_vals():
+	return (0,0)
