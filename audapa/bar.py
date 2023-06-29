f444aa84286831d1a21c919aac42c848d62569eb
bc <costin.botescu@gmail.com>
2023-06-29 07:15:51 +0300

    minimize

--- audapa/bar.py
+++ audapa/bar.py
@@ -7,6 +7,9 @@ from . import sets
 from . import play
 from . import build
 
+def min(b,win):
+	win.minimize()
+	
 def cl(b,d):
 	loop.stop()
 
@@ -17,6 +20,7 @@ def init(combo):
 	box=Gtk.Box()
 	box.append(sets.colorButton(chr(input), record.start, "Record audio", input))
 	box.append(sets.colorButton(chr(0x2699), sets.sets, "Settings", combo))
+	box.append(sets.colorButton("_", min, "Minimize", combo[0]))
 	box.append(sets.colorButton("X", cl, "Exit"))
 	box.append(play.entry)
 	box.append(play.openbutton)
